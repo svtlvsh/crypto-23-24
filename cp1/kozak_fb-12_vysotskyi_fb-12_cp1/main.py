6b4b92e630063a64684bba5a83061433b0402f38
chitahero <vladkozak2306@gmail.com>
2023-09-30 21:10:38 +0300

    text added

--- cp1/kozak_fb-12_vysotskyi_fb-12_cp1/main.py
+++ cp1/kozak_fb-12_vysotskyi_fb-12_cp1/main.py
@@ -3,8 +3,15 @@ from re import sub
 ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
 print(len(ALPHABET))
 
+path = "text.txt"
+
 def clean_text(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
         text = f.read()
     text = text.lower()
-    text = sub("[^а-яё\s]", "", text) 
+    text = sub("[^а-яё\s]", "", text)
+    text = text.replace("\n", " ")
+    print(text[0:10000]) 
+
+clean_text(path)
+
