eb5718c2bc65592aca245ec67667633589f62b9b
kpi-guy-12 <makvys-ipt25@gmail.com>
2023-10-01 14:55:07 +0300

    finished refining text

--- cp1/kozak_fb-12_vysotskyi_fb-12_cp1/main.py
+++ cp1/kozak_fb-12_vysotskyi_fb-12_cp1/main.py
@@ -1,17 +1,16 @@
 from re import sub
 
-ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
-print(len(ALPHABET))
-
 path = "text.txt"
 
 def clean_text(filepath):
     with open(filepath, "r", encoding="utf-8") as f:
         text = f.read()
     text = text.lower()
-    text = sub("[^а-яё\s]", "", text)
-    text = text.replace("\n", " ")
-    print(text[0:10000]) 
+    text = sub("[^а-яё ]", " ", text)
+    text = sub("\s+", " ", text )
+    return text
 
-clean_text(path)
+def save_text(filepath, text):
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(text)
 
