10f727148872c33e30074e33ac6aef688116162f
Артем Чавалах <artemochkin2@gmail.com>
2023-10-12 17:52:53 +0300

    second commit

--- cp1/chavalakh_fb-14_kot_fb-14_cp1/1.py
+++ cp1/chavalakh_fb-14_kot_fb-14_cp1/1.py
@@ -27,7 +27,10 @@ def calculate_frequencies(text, space = 0, cross_bigram = 0):
 
     text_length = len(text)
     letter_freq = {letter: freq / text_length for letter, freq in letter_freq.items()}
-    bigram_freq = {bigram: freq / (text_length - 1) for bigram, freq in bigram_freq.items()}
+    if cross_bigram:
+        bigram_freq = {bigram: freq / (text_length - 1) for bigram, freq in bigram_freq.items()}
+    else:
+        bigram_freq = {bigram: freq / (text_length // 2) for bigram, freq in bigram_freq.items()}
 
     return letter_freq, bigram_freq
 
@@ -37,12 +40,12 @@ def calculate_entropy_let(freq):
         entropy -= probability * math.log2(probability)
     return entropy
 
-def calculate_entropy_bigr(freq, cross = 0):
+def calculate_entropy_bigr(freq):
     entropy = 0.0
     for probability in freq.values():
         if probability > 0:
             entropy -= probability * math.log2(probability)
-    return entropy / (cross + 1)
+    return entropy / 2
     
 def calculate_redundancy(entropy, space):
     if space:
@@ -60,16 +63,17 @@ def xlsx_save(filename, letters = 1, bigrams = 1):
         for bigram, freq in bigram_freq.items():
             ws.append([bigram, freq])
 
-    wb.save(f'C:/Users/Catto/Desktop/Крипта/{filename}.xlsx')
+    wb.save(f'C:/Users/artem/Desktop/КРИПТА/{filename}.xlsx')
 
-with open(f'C:/Users/Catto/Desktop/Крипта/text.txt', 'r', encoding='utf-8') as file:
+
+with open('C:/Users/artem/Desktop/КРИПТА/text.txt', 'r', encoding='utf-8') as file:
     text = file.read()
 
 print('Letters and not cross bigrams without space:')
 letter_freq, bigram_freq = calculate_frequencies(text, 0, 0)
 xlsx_save('letters_not_cross_bigr_without_space')
 print(f'Letters entropy: {calculate_entropy_let(letter_freq)}')
-print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq, 0)}')
+print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq)}')
 print(f'Letter redundancy: {calculate_redundancy(calculate_entropy_let(letter_freq), 0)}')
 print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq), 0)}\n')
 
@@ -77,18 +81,18 @@ print('Letters and not cross bigrams with space:')
 letter_freq, bigram_freq = calculate_frequencies(text, 1, 0)
 xlsx_save('letters_not_cross_bigr_with_space')
 print(f'Letters entropy: {calculate_entropy_let(letter_freq)}')
-print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq, 0)}')
+print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq)}')
 print(f'Letter redundancy: {calculate_redundancy(calculate_entropy_let(letter_freq), 1)}')
 print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq), 1)}\n')
 
 print('Cross bigrams with space:')
 letter_freq, bigram_freq = calculate_frequencies(text, 1, 1)
 xlsx_save('Cross_bigr_with_space', 0)
-print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq, 1)}')
-print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq, 1), 1)}\n')
+print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq)}')
+print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq), 1)}\n')
 
 print('Cross bigrams without space:')
 letter_freq, bigram_freq = calculate_frequencies(text, 0, 1)
 xlsx_save('Cross_bigr_without_space', 0)
-print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq, 1)}')
-print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq, 1), 1)}\n')
+print(f'Bigrams entropy: {calculate_entropy_bigr(bigram_freq)}')
+print(f'Bigrams redundancy: {calculate_redundancy(calculate_entropy_bigr(bigram_freq), 0)}\n')
