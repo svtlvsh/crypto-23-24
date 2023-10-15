bb629d010f326f16eb6f14013eaba7296aa6aa2e
seemsgucci228 <seemsgucci228@gmail.com>
2023-10-15 20:13:43 +0300

    Correcting some mistakes

--- cp2/akhunov_fb-11_cp2/akhunov_fb-11_cp_2.py
+++ cp2/akhunov_fb-11_cp2/akhunov_fb-11_cp_2.py
@@ -58,15 +58,19 @@ def create_csv_file(filename: str, affinity_dict: dict):
         writer = csv.writer(file)
         writer.writerow(["Довжина ключа", "Індекс"])
         for affinity in affinity_dict:
-            key_len = len(affinity)
+            if affinity == 'Вхідний текст':
+                key_len = 'Вхідний текст'
+            else:
+                key_len = len(affinity)
             affinity = affinity_dict[affinity]
             writer.writerow([key_len, affinity])
 
 
 def main():
     text: str = file_read('task1.txt')
-    text: str = text_edit(text)
     affinity_dict: dict = {}
+    affinity_dict['Вхідний текст'] = affinity_index(text)
+    text: str = text_edit(text)
     for key in KEYS:
         encrypted_text = encryption(key, text)
         affinity_dict[key] = affinity_index(encrypted_text)
