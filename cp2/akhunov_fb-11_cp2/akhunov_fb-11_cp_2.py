54125e26f1fd0f6e8208779c4a31f9de76807a73
seemsgucci228 <seemsgucci228@gmail.com>
2023-10-15 19:55:52 +0300

    Creating affinity counting, csv-creator, main functions

--- cp2/akhunov_fb-11_cp2/akhunov_fb-11_cp_2.py
+++ cp2/akhunov_fb-11_cp2/akhunov_fb-11_cp_2.py
@@ -1,3 +1,5 @@
+import csv
+
 # Це ключі шифрування, довжиною 2, 3, 4, 5, 10 і 20 відповідно
 KEYS: tuple = ('да', 'дуб', 'хлеб', 'туман', 'облачность', 'завтрапятницакайфуем')
 
@@ -35,6 +37,42 @@ def encryption(key: str, input_text: str) -> str:
     return encrypted_text
 
 
-# text = file_read('task1.txt')
-# text = text_edit(text)
-# print(encryption(KEYS[5], text))
+# Ця функція рахує індекси відповідності
+def affinity_index(encrypted_text: str) -> float:
+    frequency: dict = {}
+    affinity: float = 0.0
+    for char in encrypted_text:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 0
+    for char in frequency:
+        affinity += (frequency[char] * (frequency[char] - 1)) / \
+                    (len(encrypted_text) * len(encrypted_text) - 1)
+    return affinity
+
+
+# Ця функція створює csv файл з результатами
+def create_csv_file(filename: str, affinity_dict: dict):
+    with open(filename, 'w', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Довжина ключа", "Індекс"])
+        for affinity in affinity_dict:
+            key_len = len(affinity)
+            affinity = affinity_dict[affinity]
+            writer.writerow([key_len, affinity])
+
+
+def main():
+    text: str = file_read('task1.txt')
+    text: str = text_edit(text)
+    affinity_dict: dict = {}
+    for key in KEYS:
+        encrypted_text = encryption(key, text)
+        affinity_dict[key] = affinity_index(encrypted_text)
+    print(affinity_dict)
+    create_csv_file('Affinities.csv', affinity_dict)
+
+
+if __name__ == "__main__":
+    main()
