7b05e051d4d2c85c0dfa3319b5a11d3a396b8b17
YatsentiukAndrii <andyace-ipt25@lll.kpi.ua>
2023-10-17 17:05:29 +0300

    Finished code

--- cp2/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/main3.py
+++ cp2/FB-11_Yatsentiuk_Andrii_FB-11_Kustov_Ivan/main3.py
@@ -1,4 +1,5 @@
-
+import matplotlib.pyplot as plt
+import numpy as np  # Import NumPy for array manipulation
 
 with open('message.txt', 'r', encoding = 'utf-8') as file:
     file_contents = file.read()
@@ -36,14 +37,31 @@ def Decode(text, key):
     return result
 
 
+def get_letters_with_period(input_string, period):
+    if period <= 0:
+        raise ValueError("Period should be a positive integer.")
+
+    result = input_string[::period]
+    return result
 
 
+def rotate_array(arr, rotations):
+    if len(arr) == 0:
+        return arr
+
+    rotations = rotations % len(arr)  # Ensure that rotations are within the array's length
+    return arr[-rotations:] + arr[:-rotations]
 
 
+russian_letter_frequencies = [
+    0.0801, 0.0159, 0.0454, 0.0165, 0.0298, 0.0845, 0.0072, 0.016, 0.0735,
+    0.0106, 0.0321, 0.0497, 0.0333, 0.067, 0.1097, 0.0281, 0.0473, 0.0547, 0.0626,
+    0.0262, 0.0026, 0.0097, 0.0048, 0.0144, 0.0073, 0.0061, 0.0004, 0.019, 0.0174,
+    0.0032, 0.0064, 0.0201
+]
 
 
-
-
+"""
 i = 1
 while i < 51:
     temp_text = ""
@@ -53,8 +71,42 @@ while i < 51:
         j = j + i
     print(f"Індекс для ключа {i} - ", IndexVidpovidnosty(temp_text))
     i = i + 1
-
+"""
 
 #13!!!!
+file_contents = file_contents[:]
+russian_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+string = get_letters_with_period(file_contents, 13)
+string_freq = []
+i = 0
+while i < len(russian_alphabet):
+    string_freq.append(string.count(russian_alphabet[i])/len(string))
+    i = i+1
 
+string_freq = rotate_array(string_freq, -3)
 
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ,14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+bar_width = 0.3  # Adjust this value to control the separation between bars
+x_adjusted = np.arange(len(x))
+
+# Create bar charts for both datasets
+plt.bar(x_adjusted - bar_width/2, russian_letter_frequencies, width=bar_width, label='Russian Letter Frequencies', color='b')
+plt.bar(x_adjusted + bar_width/2, string_freq, width=bar_width, label='Obtained Letter Frequencies', color='r')
+
+# Add labels and a legend
+plt.xlabel('Letter number')
+plt.ylabel('Frequency')
+plt.legend()
+
+# Display the graph
+plt.show()
+
+key = [-3,-16,-14,-12,-27,-10,-14,-2,-5,-4,-28,-12,0]
+string_key = ''
+for value in key:
+    string_key += russian_alphabet[abs(value)]
+
+print(string_key)
+
+#громыковедьма
+print(Decode(file_contents, string_key))
