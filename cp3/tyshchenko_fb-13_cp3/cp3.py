614a4c6648846c883281e6acc875ebe96437a78f
KryvavyiPotii <tishhenko.2003@gmail.com>
2023-11-02 15:54:50 +0100

    Added code and found key

--- cp3/tyshchenko_fb-13_cp3/cp3.py
+++ cp3/tyshchenko_fb-13_cp3/cp3.py
@@ -5,15 +5,21 @@ import sys
 
 ALPHABET = "абвгдежзийклмнопрстуфхцчшщыьэюя"
 M = len(ALPHABET)
+FREQ_L = "оеа"
+RARE_L = "щэф"
 BIGRAMS = ["ст", "но", "то", "на", "ен"]
 
 # Calculate gcd(a, b).
 def euclid(a: int, b: int) -> list:
-    assert a >= b, "a should not be less than b"
     # Initialize base values.
-    r = a % b
     u = [1, 0]
     v = [0, 1]
+    a0, b0 = a, b
+    if a > b:
+        a, b = b, a
+        u, v = v, u
+        a0, b0 = b, a
+    r = a % b
     q = [a // b]
     # Find gcd(a, b) and inverses.
     while a % b:
@@ -23,6 +29,10 @@ def euclid(a: int, b: int) -> list:
         u.append(u[-2] - q[-1] * u[-1])
         v.append(v[-2] - q[-1] * v[-1])
         q.append(a // b)
+    if u[-1] < 0:
+        u[-1] += b0
+    if v[-1] < 0:
+        v[-1] += a0
     # Return [gcd(a, b), a^(-1), b^(-1)].
     return [b, u[-1], v[-1]]
 
@@ -34,7 +44,6 @@ def lin_cmp(a: int, b: int, n: int):
         return (lst[2] * b) % n
     # Case gcd(a, n) = d > 1 and b % d != 0.
     if b % lst[0] != 0:
-        #print("Case gcd(a, n) = d > 1 and b % d != 0.")
         return 0
     # Case gcd(a, n) = d > 1 and b % d == 0. 
     else:
@@ -42,50 +51,84 @@ def lin_cmp(a: int, b: int, n: int):
         b //= lst[0]
         n //= lst[0]
         x = (euclid(n, a)[2] * b) % n
-        #print("Case gcd(a, n) = d > 1 and b % d == 0.")
         return [x + i * n for i in range(lst[0])]
 
 # Count bigrams of a text.
 def count_bigrams(text: str, overlap=False) -> dict:
     text_len = len(text)
-    if overlap:
-        step = 1
-    else:
-        step = 2
+    # Set step for text traversing.
+    step = overlap + 1
     bigrams = [text[s:s + 2] for s in range(0, text_len - 1, step)]
-    bigram_count = dict(sorted(Counter(bigrams).items(), reverse=True, key=lambda item: item[1]))
-    return bigram_count
+    # Return bigrams sorted by their count.
+    return dict(sorted(Counter(bigrams).items(), reverse=True, key=lambda item: item[1]))
 
-# Encode bigram.
-def encode(bigram: str) -> int:
-    encoded = ALPHABET.index(bigram[0]) * M + ALPHABET.index(bigram[1])
-    return encoded
+# Encode letter or bigram.
+def encode(ngram: str) -> int:
+    if len(ngram) == 1:
+        return ALPHABET.index(ngram)
+    else:
+        return ALPHABET.index(ngram[0]) * M + ALPHABET.index(ngram[1])
 
-# Decode bigram.
-def decode(codes) -> str:
-    second = codes % M
-    first = codes // M
-    return f"{ALPHABET[first]}{ALPHABET[second]}"
+# Decode letter or bigram.
+def decode(code) -> str:
+    # Actual decoding function.
+    def get_str(c: int) -> str: 
+        if c < M:
+            return ALPHABET[c]
+        else:
+            second = c % M
+            first = c // M
+            return f"{ALPHABET[first]}{ALPHABET[second]}"
+            
+    if isinstance(code, list):
+        return ''.join([get_str(x) for x in code])
+    else:
+        return get_str(code)
+    
+# Decrypt ciphered text.
+def decrypt(ct: str, key: tuple) -> str:
+    pt = ''
+    for i in range(0, len(ct), 2):
+        pt = pt + decode(lin_cmp(key[0], encode(ct[i:i+2]) - key[1], M ** 2))
+    return pt
 
 # Find candidates for keys.
 def find_keys(ct: str) -> list:
     keys = []
     ct_bigrams = list(count_bigrams(ct))
-    # CORNER CASE "1-1 2-2"
-    # Solve (X* - X**)a = (Y* - Y**) mod m^2.
-    a = lin_cmp(BIGRAMS[0] - BIGRAMS[1], ct_bigrams[0] - ct_bigrams[1], M ** 2)
-    for i in a:
-        b = (ct_bigrams[0] - i * BIGRAMS[0]) % M ** 2
-        keys.append((i, b))
+    # Loop through every possible combination of X*, X**, Y*, Y**.
+    for i in ct_bigrams[:5]:
+        ct_temp = [x for x in ct_bigrams[:5] if x != i]
+        for j in ct_temp:
+            for k in BIGRAMS:
+                temp = [x for x in BIGRAMS if x != k]
+                for l in temp:
+                    a = lin_cmp(encode(k) - encode(l), encode(i) - encode(j), M ** 2)
+                    if isinstance(a, list):
+                        for n in a:
+                            b = (encode(i) - n * encode(k)) % M ** 2
+                            keys.append((n, b))
+                    else:
+                        b = (encode(i) - a * encode(k)) % M ** 2
+                        keys.append((a, b))
     return keys
 
 # Check if deciphered PT is sensible.
 def sensible(pt: str) -> int:
     score = 0
-    # COUNT LETTERS (most and least frequent)
-    bigrams = list(count_bigrams(ct, overlap=True))
-    for i in range(5):
-        if bigrams[i] == BIGRAMS[i]:
+    letters = list(dict(sorted(Counter(pt).items(), reverse=True, key=lambda item: item[1])))
+    # Check the most frequent letters.
+    for l in letters[:3]:
+    	if l in FREQ_L:
+            score += 1
+    # Check the least frequent letters.
+    for l in letters[:3]:
+    	if l in RARE_L:
+            score += 1
+    # Check the most frequent bigrams.
+    bigrams = list(count_bigrams(pt, overlap=True))[:5]
+    for b in range(5):
+        if bigrams[b] in BIGRAMS:
             score += 1
     return score 
 
@@ -101,3 +144,19 @@ print(count_bigrams("hello world! hello mum"))
 print(count_bigrams("hello world! hello mum", overlap=True))
 # Encode and decode bigram.
 print(f"{decode(63)} ({encode('вб')})")
+# Check sensibility of text.
+data = open('custom.txt', 'r').read().strip()
+print(f"sensible: score = {sensible(data)}")
+data = open('var3.txt', 'r').read().strip()
+print(f"nonsensible: score = {sensible(data)}")
+# Find canditates for keys.
+candidates = find_keys(data)
+keys = []
+for k in candidates:
+    if sensible(decrypt(data, k)) > 4:
+        keys.append(k)
+# Remove duplicates.
+keys = list(dict.fromkeys(keys))
+print(keys)
+for k in keys:
+    print(f"key = {k}\n{decrypt(data, k)}\n\n")
