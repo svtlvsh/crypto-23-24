9f60e6dcf2efaa69acec2fbaca0f40c7cc0d96ed
SushchenkoOleksandrFB-12 <147100333+SushchenkoOleksandrFB-12@users.noreply.github.com>
2023-11-19 17:23:08 +0200

    Add files via upload

--- cp3/sushchenko_fb-12_cp3/lab3.py
+++ cp3/sushchenko_fb-12_cp3/lab3.py
@@ -13,6 +13,7 @@ def bigram_freq(text):
             freqs[bigram] = 1
 
     freqs = dict(sorted(freqs.items(), key=lambda x: x[1], reverse=True))
+    print(list(freqs.keys())[:5])
     return list(freqs.keys())[:5]
 
 
@@ -33,6 +34,17 @@ def ext_gcd(a, b):
         return gcd, y - (b // a) * x, x
 
 
+def solve_congruence(a, b, n, d):
+    if b % d == 0:
+        gcd, a_inv, y = ext_gcd(a // d, n // d)
+        x0 = (a_inv * (b // d)) % (n // d)
+
+        solutions = [(x0 + i * (n // d)) % n for i in range(d)]
+        return solutions
+    else:
+        return False
+
+
 def find_keys(X, Y):
     keys = []
     for x1 in X:
@@ -43,11 +55,17 @@ def find_keys(X, Y):
                         pass
                     else:
                         gcd, x, y = ext_gcd(x1-x2, alph_len**2)
-
                         if gcd == 1:
                             a = ((y1-y2)*x) % alph_len**2
                             b = (y1 - a*x1) % alph_len**2
                             keys.append((a, b))
+                        elif gcd > 1:
+                            result = solve_congruence((x1-x2), (y1-y2), alph_len**2, gcd)
+                            if result:
+                                for a in result:
+                                    b = (y1 - a * x1) % alph_len ** 2
+                                    keys.append((a, b))
+    print(keys)
     return keys
 
 
@@ -71,7 +89,7 @@ def decrypt(text, key):
 
 
 def find_prop_text(text):
-    denied = ["аь", "ьь", "ээ", "юф"]
+    denied = ["аь", "ьь", "ээ", "ыь", "йь", "йй", "цщ", "уь", "оь", "иь"]
     for i in denied:
         if i in text:
             return False
@@ -81,6 +99,7 @@ def find_prop_text(text):
 if __name__ == '__main__':
     with open("text.txt", "r", encoding="utf-8") as f:
         text = f.read()
+        print(text)
 
     bigram_aplh = bigram_freq(text)
     keys = find_keys(alph_to_num(bigram_top_freq), alph_to_num(bigram_aplh))
